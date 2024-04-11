512855860306f643b3284920d083a79f4d8adf52
TheSawe <maxim.polochkin1@gmail.com>
2024-04-11 22:55:10 +0300

    Added js file and getting input field value using flask

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -12,5 +12,10 @@ def homepage():
 def redirect_to_homepage():
     return redirect('/tasks/today')
 
+@app.route('/result', methods=['POST'])
+def result():
+    input_value = request.form['input_value']
+    print(input_value)
+    return redirect('/tasks/today')
 
 app.run()
