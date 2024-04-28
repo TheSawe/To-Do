0299781e76fe25dbbabd622a9d2e8bfb024afcbb
TheSawe <maxim.polochkin1@gmail.com>
2024-04-28 05:21:12 +0300

    Fixed bug about deleting all tasks with same text

--- App.py
+++ App.py
@@ -18,46 +18,56 @@ def registration():
     return render_template('start_with.html', identifier=0)
 
 
-@app.route('/request-to-register', methods=['POST'])
+@app.route('/request-to-register', methods=['POST', 'GET'])
 def requets_to_register():
-    name = request.form['name']
-    email = request.form['email']
-    password = request.form['password']
-    if valid_data(name=name, email=email, password=password):
-        with connection.cursor() as cursor:
-            insert_query = f"INSERT INTO to_do_users (name, email, password) VALUES ('{name}', '{email}', '{hash(password)}');"
-            cursor.execute(insert_query)
-            connection.commit()
-        return redirect('/sign-in')
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        if valid_data(name=name, email=email, password=password):
+            with connection.cursor() as cursor:
+                insert_query = f"INSERT INTO to_do_users (name, email, password) VALUES ('{name}', '{email}', '{hash(password)}');"
+                cursor.execute(insert_query)
+                connection.commit()
+            return redirect('/sign-in')
+        else:
+            return redirect('/register')
     else:
-        return redirect('/register')
+        return redirect('/tasks/today')
 
 @app.route('/sign-in')
 def sign_in():
+    if request.cookies.get('personal_data'):
+        return redirect('/tasks/today')
     return render_template('start_with.html', identifier=1)
 
-@app.route('/request-to-sign-in', methods=['POST'])
+@app.route('/request-to-sign-in', methods=['POST', 'GET'])
 def request_to_sign_in():
-    name = request.form['name']
-    password = request.form['password']
-    with connection.cursor() as cursor:
-        select_all_rows = "SELECT * FROM `to_do_users`"
-        cursor.execute(select_all_rows)
-        rows = cursor.fetchall()
-        for row in rows:
-            if row['name'] == name and row['password'] == hash(password):
-                return redirect(f'/cookie/{name}/{hash(password)}')
-    return redirect('/sign-in')
+    if request.method == 'POST':
+        name = request.form['name']
+        password = request.form['password']
+        with connection.cursor() as cursor:
+            select_all_rows = "SELECT * FROM `to_do_users`"
+            cursor.execute(select_all_rows)
+            rows = cursor.fetchall()
+            for row in rows:
+                if row['name'] == name and row['password'] == hash(password):
+                    return redirect(f'/cookie/{name}/{hash(password)}')
+        return redirect('/sign-in')
+    else:
+        return redirect('/tasks/today')
     
 
 @app.route('/tasks/today')
 def homepage():
-    with connection.cursor() as cursor:
-        select_rows = f"SELECT * FROM `tasks` where owner='{request.cookies.get('personal_data').split()[0][5:]}'"
-        cursor.execute(select_rows)
-        rows = cursor.fetchall()
-    return render_template('main.html', action='Мой день', tasks_length=len(rows), tasks=rows, date=get_current_date())
-
+    try:
+        with connection.cursor() as cursor:
+            select_rows = f"SELECT * FROM `tasks` where owner='{request.cookies.get('personal_data').split()[0][5:]}'"
+            cursor.execute(select_rows)
+            rows = cursor.fetchall()
+        return render_template('main.html', action='Мой день', tasks_length=len(rows), tasks=rows, date=get_current_date())
+    except:
+        return redirect('/sign-in')
 
 @app.route('/', methods=['POST', 'GET'])
 def redirect_to_homepage():
@@ -76,6 +86,7 @@ def cookie(name, password):
     return res
 
 
+
 @app.route('/result', methods=['POST'])
 def result():
     input_value = request.form['input_value']
@@ -90,7 +101,7 @@ def result():
 @app.route('/delete-task/<task_route>', methods=['POST'])
 def delete_task(task_route):
     with connection.cursor() as cursor:
-        delete_query = f'DELETE from `tasks` where task="{task_route}";'
+        delete_query = f'DELETE from `tasks` where task="{task_route}" LIMIT 1;'
         cursor.execute(delete_query)
         connection.commit()
     return redirect('/tasks/today')
